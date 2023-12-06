67a975696a500ace1b9b0de28be6f3e123e79829
veganbeef <veganbeef@protonmail.com>
2023-12-06 10:54:59 -0800

    added backup url

--- frontend/app/constants.ts
+++ frontend/app/constants.ts
@@ -1,2 +1,3 @@
-const LOCALHOST_BASE_URL = 'http://localhost:8000/api/comics/'
-export const BASE_URL = process.env.API_BASE_URL ?? LOCALHOST_BASE_URL;
+const LOCALHOST_BASE_URL = 'http://localhost:8000/api/comics/';
+const API_BSAE_URL = 'https://veganbeef.pythonanywhere.com/api/comics/';
+export const BASE_URL = process.env.API_BASE_URL ?? API_BSAE_URL ?? LOCALHOST_BASE_URL;
